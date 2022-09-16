31799741e1784af54acf683bc45bb2a2db8eb0c9
Douglas Scaini <douglasscaini.dev@gmail.com>
2022-09-15 22:04:39 -0300

    Product Page + SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/future/image";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
@@ -13,7 +14,7 @@ interface HomeProps {
         id: string;
         name: string;
         imageUrl: string;
-        price: number;
+        price: string;
     }[];
 }
 
@@ -28,14 +29,16 @@ export default function Home({ products }: HomeProps) {
     return (
         <HomeContainer ref={sliderRef} className="keen-slider">
             {products.map(product => (
-                <Product key={product.id} className="keen-slider__slide">
-                    <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+                    <Product className="keen-slider__slide">
+                        <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-                    <footer>
-                        <strong>{product.name}</strong>
-                        <span>{product.price}</span>
-                    </footer>
-                </Product>
+                        <footer>
+                            <strong>{product.name}</strong>
+                            <span>{product.price}</span>
+                        </footer>
+                    </Product>
+                </Link>
             ))}
         </HomeContainer>
     );
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,72 @@
-export default function Product() {
-    return <h1>Product!</h1>;
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/future/image";
+
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+    };
 }
+
+export default function Product({ product }: ProductProps) {
+    return (
+        <ProductContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={580} alt="" />
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+                <p>{product.description}</p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+    return {
+        paths: [
+            { params: { id: "prod_MQxdKUp913dUNE" } },
+            { params: { id: "prod_MQxdyl2WxrRFmT" } },
+            { params: { id: "prod_MQxcNsUly3kQ3u" } },
+            { params: { id: "prod_MQxbhiFgRIbKza" } },
+        ],
+        fallback: "blocking",
+    };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+    const productId = params.id;
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ["default_price"],
+    });
+
+    const price = product.default_price as Stripe.Price;
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                name: product.name,
+                imageUrl: product.images[0],
+                price: new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                }).format(price.unit_amount / 100),
+                description: product.description,
+            },
+        },
+        revalidate: 60 * 60 * 24, // 24 hours
+    };
+};
